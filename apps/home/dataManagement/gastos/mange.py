902587af0f2434c51ac28c9742108c8ce8375dc8
jjsanchezc <jjsanchezc@eafit.edu.co>
2022-04-01 03:28:37 -0500

    se agregó el espacio para los ingresos, donde leen los datos (BETA) y donde llama una funcion que vamos a usar para conectarla con la base de datos

---
+++ apps/home/dataManagement/gastos/mange.py
@@ -0,0 +1,1 @@
+##este metodo va a ser practicamente lo mismo que el mamange de ingresos
---
+++ apps/home/dataManagement/ingresos/manage.py
@@ -0,0 +1,11 @@
+def ingresos(origen,des,cant,):
+    """metodo para registrar los ingresos
+
+    Args:
+        origen (float): datos de la persona o empresa que envió el dinero
+        des (str): algunos comentarios opcionales que puede poner el usuario
+        cant (float): cantidad de dinero enviado
+    """
+    print("------------- \n ---------------")
+    print(f"origen: {origen} \nDescripcion: {des} \nCantidad: {cant}")
+    
